f3c22833383d178e7b7c4d042e29628bdee27416
george1605 <32997165+george1605@users.noreply.github.com>
2022-04-05 20:27:25 +0300

    Added typeof() and JS types in C++

--- exec.cpp
+++ exec.cpp
@@ -1,4 +1,18 @@
 #include <string.h>
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+
+enum types { ARRAY, OBJECT, STRING, NUMBER, NAN, UNKNOWN }; // JS types
+enum types typeof(std::string i)
+{
+  if(i[0] == '[' && *i.end() == ']')
+    return types::ARRAY;
+  else if(atoi(i) != 0)
+    return types::NUMBER;
+  else
+    return types::UNKNOWN;
+}
 
 void exec(std::string x)
 {
